616c09cd8e9e93a5f94c05895b645550a1c8c3a9
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-11-25 08:22:28 -0300

    Aula 20 - Persistência dos dados [ Parte 1 ] - finalizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from 'process';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
+///npm install @nestjs/mongoose mongoose
+///npm install -D @types/mongoose
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(env.url_connection, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/jogadores/interfaces/jogador.interface.ts
+++ src/jogadores/interfaces/jogador.interface.ts
@@ -1,4 +1,8 @@
-export interface JogadorInterface {
+import { Document } from 'mongoose';
+
+///O document traz várias facilidades do modelo do
+///MongoDB, como alguns métodos para persistência e consulta no banco
+export interface JogadorInterface extends Document {
   readonly _id: string;
   readonly telefoneCelular: string;
   readonly email: string;
